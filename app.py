cef1e8541baa6b9f36eeb67749c0c5ebd3b8dedd
KashviBhandari <kashvibhandari76@gmail.com>
2026-06-04 15:12:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -64,8 +64,10 @@ st.markdown("""
 h1,h2,h3,h4,h5,h6{
     color:#FFFFFF !important;
 }
-
-
+/* Text */
+p,span,label,div{
+    color:#FFFFFF;
+}
 
 /* ==========================================
    SIDEBAR MAIN
