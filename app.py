e580d56d937c895b01734470b4bd3550778b171a
KashviBhandari <kashvibhandari76@gmail.com>
2026-06-11 16:25:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -765,7 +765,7 @@ if st.sidebar.button("Logout"):
 
 if menu == "Live Market":
 
-        st.title(Live Market Dashboard")
+        st.title("Live Market Dashboard")
         
 
         # ================= CACHE DATA =================
